b6ce0f8c9abb8d95f1f3c448feff89c109eababc
Santiago <santiagorl229@gmail.como>
2021-11-14 10:27:20 -0500

    Inicar el video 11 del modulo 5

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
 
 @Component({
   selector: 'app-main-page',
@@ -6,7 +8,28 @@ import { Component} from '@angular/core';
 })
 export class MainPageComponent {
 
-  agregar(){
-    console.log('Hey!!')
+  personajes: Personaje[]=[
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500
+    }
+  ];
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
   }
+  agregar(){
+    if(this.nuevo.nombre.trim().length===0){
+      return;
+    }
+    this.personajes.push(this.nuevo)
+    this.nuevo = {nombre:'',poder:0}  
+  }
+  // cambiarNombre(event: any){
+  //   console.log(event.target.value)
+  // }
 }
