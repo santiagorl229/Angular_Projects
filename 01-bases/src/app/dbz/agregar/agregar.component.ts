7a39ada9262504c633301af1202c02cea88f09c8
Santiago Rodriguez López <srodriguezl@sistecredito.com>
2022-01-19 14:32:59 -0500

    Finalizamos el módulo(Seccion 5)

--- 01-bases/src/app/dbz/agregar/agregar.component.ts
+++ 01-bases/src/app/dbz/agregar/agregar.component.ts
@@ -1,5 +1,6 @@
 import { Component,EventEmitter, Input, Output } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-agregar',
@@ -11,14 +12,16 @@ export class AgregarComponent {
     nombre: '',
     poder: 0
   }
+  constructor(private dbzService: DbzService){}
 
-  @Output() OnNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+  // @Output() OnNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
 
   agregar(){
     if(this.nuevo.nombre.trim().length===0){
       return;
     }
-    this.OnNuevoPersonaje.emit(this.nuevo);
+    this.dbzService.agregarPersonaje(this.nuevo);
+    // this.OnNuevoPersonaje.emit(this.nuevo);
     this.nuevo = {nombre:'',poder:0}  
   }  
 }
--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 
 @Component({
@@ -7,25 +8,19 @@ import { Personaje } from '../interfaces/dbz.interface';
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
-
-  personajes: Personaje[]=[
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ];
+  
+  
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 1000
   }
-  agregarNuevoPersonaje(argumento: Personaje){
-    this.personajes.push(argumento);
-  }
-  // cambiarNombre(event: any){
-  //   console.log(event.target.value)
+ 
+  
+  // agregarNuevoPersonaje(argumento: Personaje){
+  //   this.personajes.push(argumento);
   // }
+  constructor(private dbzService: DbzService){
+    // this.personajes = this.dbzService.personajes;
+  }
+
 }
